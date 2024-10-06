ec310ee04d702e727d03078e671336ed3631632e
Trung Tran <trungtran63470@gmail.com>
2024-10-05 20:50:02 -0700

    prettified

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,30 +5,30 @@ const config: Config = {
   theme: {
     extend: {
       keyframes: {
-        'fade-in-top': {
-          '0%': {
-            transform: 'translateY(-50px)',
-            opacity: '0',
+        "fade-in-top": {
+          "0%": {
+            transform: "translateY(-50px)",
+            opacity: "0",
           },
-          '100%': {
-            transform: 'translateY(0)',
-            opacity: '1',
+          "100%": {
+            transform: "translateY(0)",
+            opacity: "1",
           },
         },
-        'fade-in-bottom': {
-          '0%': {
-            transform: 'translateY(30px)',
-            opacity: '0',
+        "fade-in-bottom": {
+          "0%": {
+            transform: "translateY(30px)",
+            opacity: "0",
           },
-          '100%': {
-            transform: 'translateX(0)',
-            opacity: '1',
+          "100%": {
+            transform: "translateX(0)",
+            opacity: "1",
           },
         },
       },
       animation: {
-        'fade-in-top': 'fade-in-top 0.7s ease-out forwards',
-        'fade-in-bottom': 'fade-in-bottom 0.7s ease-out forwards',
+        "fade-in-top": "fade-in-top 0.7s ease-out forwards",
+        "fade-in-bottom": "fade-in-bottom 0.7s ease-out forwards",
       },
     },
   },
