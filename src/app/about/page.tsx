cd64bd2b9af4303671c4ba24dff290c822cceed9
pynappo <lehtien.david@gmail.com>
2024-10-06 04:37:32 -0700

    Add initial officer cards/officers component

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+import Officers from "../components/organisms/officers";
+export default function About() {
+  return (
+    <div className="about">
+      <h2>Officers</h2>
+      <Officers />
+    </div>
+  );
+}
---
+++ src/app/components/organisms/officers.tsx
@@ -0,0 +1,25 @@
+import OfficerCard, { Officer } from "../molecules/officer-card";
+import { promises as fs } from "fs";
+
+export default async function Officers() {
+  const file = await fs.readFile(
+    process.cwd() + "/src/app/assets/data.json",
+    "utf8",
+  );
+  const data = JSON.parse(file);
+  const officers: Officer[] = data.officers;
+  return (
+    <div className="officer-cards flex flex-wrap justify-center gap-4">
+      {officers.map((officer, i) => {
+        return (
+          <OfficerCard
+            key={i}
+            name={officer.name}
+            avatarSrc={officer.avatarSrc}
+            role={officer.role}
+          />
+        );
+      })}
+    </div>
+  );
+}
