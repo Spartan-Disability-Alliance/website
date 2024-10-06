1f2ab88ccec4a765df53c6d7213fb6637140466d
Naya Singhania <naya.singhania@gmail.com>
2024-10-05 19:48:48 -0700

    Update footer.tsx

--- src/app/components/organisms/footer.tsx
+++ src/app/components/organisms/footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-base-300 text-right py-4">
+    <footer className="bg-base-300 text-center py-4">
       <div className="container mx-auto">
         <p className="text-sm">
           &copy; Copyright 2024 Spartan Disability Alliance
