f13720d2dae7f6c6602f8dabb3ba95a167e61fd7
pynappo <lehtien.david@gmail.com>
2024-10-09 22:44:46 -0700

    Add heading

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,9 @@
 import Officers from "../components/organisms/officers";
+import Heading from "../components/atoms/heading";
 export default function About() {
   return (
-    <div className="about">
-      <h2>Officers</h2>
+    <div className="about py-10">
+      <Heading>Officers</Heading>
       <Officers width={"60rem"} />
     </div>
   );
