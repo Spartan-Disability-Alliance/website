532a00b95927f011f8a43a90a86b8aff259a37c4
Trung Tran <trungtran63470@gmail.com>
2024-10-09 22:15:32 -0700

    add fade-in animation

--- src/app/components/molecules/social-btn.tsx
+++ src/app/components/molecules/social-btn.tsx
@@ -15,7 +15,7 @@ const SocialBtn: React.FC<SocialBtnProps> = ({ href, icon, text }) => {
       <a target="_blank" rel="noopener noreferrer" className="w-full">
         <Button
           onClick={() => {}}
-          className={`h-20 w-full flex items-center justify-center space-x-2 rounded-lg border-none bg-gray-800 hover:scale-110 ease-out duration-300 hover:bg-gray-700`}
+          className={`h-20 w-full flex items-center justify-center space-x-2 rounded-lg border-none bg-gray-800 hover:scale-110 ease-out duration-300 hover:bg-gray-700 animate-fade-in-bottom`}
         >
           <span className={`icon`} dangerouslySetInnerHTML={{ __html: icon }} />
           <span>{text}</span>
